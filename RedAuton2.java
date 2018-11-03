f302854fed5566101f45735426b4c02e23c2ef53
Joey <josephwillenbucher@hunschool.org>
2018-11-03 09:19:11 -0400

    changed to java

--- RedAuton2.java
+++ RedAuton2.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name = "RedAuton2", group = "Linear")
 public class RedAuton2 extends LinearOpMode {
