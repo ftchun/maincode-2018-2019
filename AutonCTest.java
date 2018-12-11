24596a91e20264d21ef9395304278089bba69f73
Eric <ericwu@hunschool.org>
2018-12-11 17:00:19 -0500

    I added a new file called "AutonCTest" which contains some code that might be able to run our AutonC without the long pauses after the move methods. Only this method was changed.

---
+++ AutonCTest.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name = "AutonC", group = "Linear")
+public class AutonC extends LinearOpMode {
+
+    private DcMotor motorFL;
+    private DcMotor motorFR;
+    private DcMotor motorBL;
+    private DcMotor motorBR;
+	private Servo servo;
+
+    private DcMotor motorHR;
+    private DcMotor motorHL;
+	
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+		servo = hardwareMap.servo.get("servo");
+        motorBL.setDirection(DcMotor.Direction.REVERSE);
+        motorFL.setDirection(DcMotor.Direction.REVERSE);
+
+        motorHL = hardwareMap.dcMotor.get("motorHL");
+        motorHR = hardwareMap.dcMotor.get("motorHR");
+
+        motorHL.setDirection(DcMotor.Direction.REVERSE);
+
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorHL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorHL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorHR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorHR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        waitForStart();
+
+        moveLift(1500, 1500, 1, 5000);
+
+        shiftRight(3000, .5, 4000);
+        //moveRightWheels(-3000, .5, 5000);
+
+        move(-500, -.5, 1500);
+
+        shiftRight(1500, .5, 1500);
+
+        move(4500, .5, 5000);
+    }
+
+    public void move(int distance, double power, long time) throws InterruptedException {
+        //this is the only function that was changed.
+        //instead of waiting for a specified amount of time,
+        //we wait until both motors are not "busy"
+        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorBL.setTargetPosition(distance);
+        motorBR.setTargetPosition(distance);
+
+        motorBL.setPower(power);
+        motorBR.setPower(power);
+
+        while(motorBL.isBusy() && motorBR.isBusy()){
+        	timer(1);
+        }
+
+        motorBL.setPower(0);
+        motorBR.setPower(0);
+
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void shiftRight(int distance, double power, long time) throws InterruptedException {
+        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorBL.setTargetPosition(distance);
+        motorBR.setTargetPosition(-distance);
+
+        motorBL.setPower(power);
+        motorBR.setPower(power);
+
+        timer(time);
+
+        motorBL.setPower(0);
+        motorBR.setPower(0);
+
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void moveRightWheels(int distance, double power, long time) throws InterruptedException {
+        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        motorBR.setTargetPosition(distance);
+
+        motorBR.setPower(power);
+
+        timer(time);
+
+        motorBR.setPower(0);
+
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void moveLift(int positionL, int positionR, double power, long time) throws InterruptedException {
+        motorHL.setTargetPosition(positionL);
+        motorHR.setTargetPosition(positionR);
+        motorHL.setPower(power);
+        motorHR.setPower(power);
+
+        timer(time);
+    }
+
+	private void timer(long time) throws InterruptedException {
+        Thread.sleep(time);
+    }
+}
